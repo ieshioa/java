c947403280996a35dd9685bd456384bc1d19be60
Administrator <mj17428@naver.com>
2024-03-15 17:45:38 +0900

    first

--- src/main/java/com/green/day1/Memo.java
+++ src/main/java/com/green/day1/Memo.java
@@ -1,8 +1,8 @@
 package com.green.day1;
 
 public class Memo {
-    public static void main(String[] args){  // psvm
-        System.out.print("Hello!\n"); // "" 문자열, String
+    public static void main(String[] args){
+        System.out.print("Hello!\n");
     }
 
 }
@@ -10,52 +10,48 @@ public class Memo {
 /*
 
 "" : 문자열, String
+void : 리턴타입
+main : 메소드명
+() : 매개변수
+int : 데이터타입
+
+
+[단축키]
+ctrl + / : 여러줄 한번에 주석
 ctrl+d : 줄복사
+sout : System.out.println();
+psvm : public static void main(String[] args) {}
 
-
-sout =
-System.out.println();
-
-psvm =
-public static void main(String[] args) {
-    }
-void 리턴타입
-main 메소드명
-() 매개변수
-int 데이터타입
-
-
- 안에: 메소드, 전역변수
-객체 구성요소 : 속성(명사), 메소드(동사)
-
+[객체 구성요소]
+속성(명사), 메소드(동사)
 
 static : 객체와 상관 없음
+
 변수명 : 숫자로 시작하면 안됨
         _ $ 가능
 
 전역변수: 초기화 안해도 됨, 디폴트값이 있음
 지역변수: 무조건 초기화 필수
 
-데이터타입
+[데이터타입]
 정수형: int
 실수형: double
 불린형: boolean (true, false 값만 가질 수 있는 데이터타입)
 
-형변환
+[형변환]
 타입이 안맞으면 맞게 타입 변화를 줘야한다.
 자동형변환, 강제형변환
 
-랜덤변수
-Math.random()
-0.00000 ~ 0.9999
+[랜덤변수]
+Math.random()  // 0.00000 ~ 0.9999
+Math.random() * 10  // 0 ~ 9
 int num = (int)(Math.random() * 10) + 1;   // 1 ~ 10
 
-
-삼항식, 조건식
+[삼항식]
+조건식
 식 ? true일 때 : false일 때;
 
 
-여러줄 한번에 주석
-ctrl + /
+
 
  */
