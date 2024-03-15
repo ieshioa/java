b39c92a8dcf3cba19e32b6ef3efc7273f2fadc8b
Administrator <mj17428@naver.com>
2024-03-15 14:15:01 +0900

    first

--- src/main/java/com/green/day2/CompOperator.java
+++ src/main/java/com/green/day2/CompOperator.java
@@ -6,13 +6,27 @@ public class CompOperator {
         boolean b1 = true;
         boolean b2 = false;
 
-        boolean r1 = 10 > 9;
+        boolean r1 = 10 > 9;  // 10은 9보다 초과?
         System.out.println(r1);
 
-        r1 = 10 < 9;
+        r1 = 10 < 9;  // 10은 9보다 미만?
         System.out.println(r1);
 
-        r1 = 10 >= 9;
+        r1 = 10 >= 9;  // 10은 9보다 이상?
         System.out.println(r1);
+
+        r1 = 10 >= 10;  // 10은 10보다 이상?
+        System.out.println(r1);
+
+        r1 = 10 <= 9;  // 10은 9보다 이하?
+        System.out.println(r1);
+
+        r1 = 10 == 10;  // 10은 10과 같나?
+        System.out.println(r1);
+
+        r1 = 10 != 10;  // 10은 10과 다른가?
+        System.out.println(r1);
+
+
     }
 }
